1bd751017c170df38a4ed399abf301a7b63de58d
James William Fletcher <78346668+mrbid@users.noreply.github.com>
2024-01-21 05:40:32 +0000

    Update main.c

--- main.c
+++ main.c
@@ -23,7 +23,7 @@
 const char appTitle[]="PLY Viewer";
 uint winw=1024, winh=768;
 GLFWwindow* wnd;
-mat projection, model;\
+mat projection, model;
 void updateModel(){glUniformMatrix4fv(modelview_id, 1, GL_FALSE, (float*)&model.m[0][0]);}
 
 // load model from file to gpu memory with a permanent 12 MB staging buffer
